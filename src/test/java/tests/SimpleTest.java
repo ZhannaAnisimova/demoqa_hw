2e76e3762bb1b98d96aeb7e3aa72983b40e224aa
Zhanna Anisimova <zhzhv@yandex.ru>
2025-03-21 20:32:45 +0300

    added corrections and picture

--- src/test/java/tests/SimpleTest.java
+++ src/test/java/tests/SimpleTest.java
@@ -7,9 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-
+import static com.codeborne.selenide.Selenide.*;
 
 
 public class SimpleTest {
@@ -26,6 +24,8 @@ public class SimpleTest {
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
         $("#firstName").setValue("Zhanna");
         $("#lastName").setValue("Zhirnova");
         $("#userEmail").setValue("zhzhv@yandex.ru");
@@ -37,7 +37,7 @@ public class SimpleTest {
         $(".react-datepicker__day--016").click();
         $("#subjectsInput").setValue("English").pressEnter();
         $("[for = 'hobbies-checkbox-1']").click();
-        $("#uploadPicture").uploadFile(new File("C:/Users/Zhanna/Desktop/cat.jpg"));
+        $("#uploadPicture").uploadFromClasspath("cat.jpg");
         $("#currentAddress").setValue("Tver, street 66");
         $("#react-select-3-input").setValue("NCR").pressEnter();
         $("#react-select-4-input").setValue("Noida").pressEnter();
